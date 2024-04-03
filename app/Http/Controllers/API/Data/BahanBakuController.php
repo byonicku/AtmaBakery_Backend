f443fe349ebcac36ebc0053e4c405444a57c7e26
Nico Herlim <nicoherlim2003@gmail.com>
2024-04-03 18:43:55 +0700

    add bahan baku controller

---
+++ app/Http/Controllers/API/Data/BahanBakuController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Http\Controllers\API\Data;
+
+use App\Models\BahanBaku;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class BahanBakuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = BahanBaku::all();
+
+        if (count($data) == 0) {
+            return response()->json([
+                'message' => 'Data is empty',
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Data successfully retrieved',
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'nama_bahan_baku' => 'required|max:255',
+            'satuan' => 'required|max:255',
+            'stok' => 'required|min:0',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'message' => $validate->errors()->first(),
+            ], 400);
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $data = BahanBaku::create([
+                'nama_bahan_baku' => $request->nama_bahan_baku,
+                'satuan' => $request->satuan,
+                'stok' => $request->stok,
+            ]);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Data successfully created',
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $data = BahanBaku::find($id);
+
+        if (!$data) {
+            return response()->json([
+                'message' => 'Data not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Data successfully retrieved',
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = BahanBaku::find($id);
+
+        if (!$data) {
+            return response()->json([
+                'message' => 'Data not found',
+            ], 404);
+        }
+
+        $validate = Validator::make($request->all(), [
+            'nama_bahan_baku' => 'sometimes|max:255',
+            'satuan' => 'sometimes|max:255',
+            'stok' => 'sometimes|min:0',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'message' => $validate->errors()->first(),
+            ], 400);
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $data->update([
+                'nama_bahan_baku' => $request->nama_bahan_baku,
+                'satuan' => $request->satuan,
+                'stok' => $request->stok,
+            ]);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Data successfully updated',
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $data = BahanBaku::find($id);
+
+        if (!$data) {
+            return response()->json([
+                'message' => 'Data not found',
+            ], 404);
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $data->delete();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Data successfully deleted',
+        ], 200);
+    }
+}
