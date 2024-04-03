9fedb4876c1d1988a84dcf6bacdbb7f556335014
Nico Herlim <nicoherlim2003@gmail.com>
2024-04-03 11:02:24 +0700

    cleaning web route

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/verify/{verify_key}', [App\Http\Controllers\API\Auth\UserAuthController::class, 'verify'])->name('verify');
-
-Route::get('/verify/failed', function () {
-    return view('FailedVerify');
-});
-
-Route::get('/verify/success', function () {
-    return view('SuccessVerify');
-});
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/verify/{verify_key}', [UserAuthController::class, 'verify'])->name('verify');
