d889cc102fe082b34eb6e0dafbd5bd1808042861
Nico Herlim <nicoherlim2003@gmail.com>
2024-05-07 17:18:23 +0700

    fix data in detail hampers

--- app/Models/DetailHampers.php
+++ app/Models/DetailHampers.php
@@ -19,16 +19,16 @@ class DetailHampers extends Model
 
     public function hampers()
     {
-        return $this->belongsTo(Hampers::class, 'id_hampers', 'id_hampers');
+        return $this->belongsTo(Hampers::class, 'id_hampers', 'id_hampers')->withTrashed();
     }
 
     public function produk()
     {
-        return $this->belongsTo(Produk::class, 'id_produk', 'id_produk');
+        return $this->belongsTo(Produk::class, 'id_produk', 'id_produk')->withTrashed();
     }
 
     public function bahan_baku()
     {
-        return $this->belongsTo(BahanBaku::class, 'id_bahan_baku', 'id_bahan_baku');
+        return $this->belongsTo(BahanBaku::class, 'id_bahan_baku', 'id_bahan_baku')->withTrashed();
     }
 }
