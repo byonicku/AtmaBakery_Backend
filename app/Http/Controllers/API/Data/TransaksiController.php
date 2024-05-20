bfe53131ed2569e5b1a797ef0f5e66292ce63cc1
Nico Herlim <nicoherlim2003@gmail.com>
2024-05-20 21:00:50 +0700

    coba benerin search all

--- app/Http/Controllers/API/Data/TransaksiController.php
+++ app/Http/Controllers/API/Data/TransaksiController.php
@@ -518,30 +518,42 @@ class TransaksiController extends Controller
         }
 
         if ($request->query('status')) {
-            $transaksi = Transaksi::with('user', 'alamat')
+            $transaksi = Transaksi::with('detail_transaksi.produk', 'detail_transaksi.hampers')
                 ->where('status', '=', $request->query('status'))
-                ->whereAny([
-                    'no_nota',
-                    'tanggal_pesan',
-                    'tanggal_lunas',
-                    'tanggal_ambil',
-                    'total',
-                    'tipe_delivery',
-                    'status',
-                ], 'LIKE', '%' . $data . '%')
+                ->whereHas('detail_transaksi', function ($query) use ($data) {
+                    $query->whereHas('produk', function ($query) use ($data) {
+                        $query->where('nama_produk', 'LIKE', '%' . $data . '%');
+                    })->orWhereHas('hampers', function ($query) use ($data) {
+                        $query->where('nama_hampers', 'LIKE', '%' . $data . '%');
+                    });
+                })->whereAny([
+                        'no_nota',
+                        'tanggal_pesan',
+                        'tanggal_lunas',
+                        'tanggal_ambil',
+                        'total',
+                        'tipe_delivery',
+                        'status',
+                    ], 'LIKE', '%' . $data . '%')
                 ->orderByDesc('no_nota')
                 ->get();
         } else {
-            $transaksi = Transaksi::with('user', 'alamat')
-                ->whereAny([
-                    'no_nota',
-                    'tanggal_pesan',
-                    'tanggal_lunas',
-                    'tanggal_ambil',
-                    'total',
-                    'tipe_delivery',
-                    'status',
-                ], 'LIKE', '%' . $data . '%')
+            $transaksi = Transaksi::with('detail_transaksi.produk', 'detail_transaksi.hampers')
+                ->whereHas('detail_transaksi', function ($query) use ($data) {
+                    $query->whereHas('produk', function ($query) use ($data) {
+                        $query->where('nama_produk', 'LIKE', '%' . $data . '%');
+                    })->orWhereHas('hampers', function ($query) use ($data) {
+                        $query->where('nama_hampers', 'LIKE', '%' . $data . '%');
+                    });
+                })->whereAny([
+                        'no_nota',
+                        'tanggal_pesan',
+                        'tanggal_lunas',
+                        'tanggal_ambil',
+                        'total',
+                        'tipe_delivery',
+                        'status',
+                    ], 'LIKE', '%' . $data . '%')
                 ->orderByDesc('no_nota')
                 ->get();
         }
