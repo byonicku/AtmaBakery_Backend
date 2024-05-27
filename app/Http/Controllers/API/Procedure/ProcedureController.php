a7cdb8970df8ae657593410bac65aeabfe56c132
Nico Herlim <nicoherlim2003@gmail.com>
2024-05-27 22:37:09 +0700

    untuk ngoding dulu nanti di revert

--- app/Http/Controllers/API/Procedure/ProcedureController.php
+++ app/Http/Controllers/API/Procedure/ProcedureController.php
@@ -516,7 +516,7 @@ class ProcedureController extends Controller
         try {
             $result = Transaksi::with('detail_transaksi.produk', 'detail_transaksi.hampers')
                 ->whereDate('tanggal_ambil', '=', '2024-05-24')
-                ->where('status', '=', 'Pesanan Diterima');
+                ->where('status', '=', 'Ditolak');
         } catch (\Exception $e) {
             return response()->json([
                 'message' => $e->getMessage(),
